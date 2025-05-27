eb40cc1de22e4c8e2d842f55a962141a29898dc7
Srikantdoddi <doddisrikanth7931@gmail.com>
2025-05-27 14:57:48 +0530

    Update: Modified Offers component and config files

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-// import { StrictMode } from 'react';
-// import { createRoot } from 'react-dom/client';
-// import React from 'react';
-// import './index.css'
-// import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+import './index.css'
+import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-import { BrowserRouter } from 'react-router-dom';
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
-<BrowserRouter basename="/Restaurent-Booking">
-  <App />
-</BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/Restaurent-Booking/', // <- important!
+  base: './', // <- important!
 
   plugins: [
-    // tailwindcss(),
+    tailwindcss(),
     react()
   ],
 })
